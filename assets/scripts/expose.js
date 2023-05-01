0609ed3cd209f28e2d323771efb5bc0e2581f17a
Jiecheng Chen <jasonarong96@gmail.com>
2023-05-01 16:26:48 -0700

    On option change, change img and audio src

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,18 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // getting HTML elements
+  const hornImg = document.querySelector('img');
+  const selection = document.getElementById('horn-select');
+  const control = document.getElementById('volume-controls');
+  const audio = document.querySelector('audio');
+
+  // When the selected option changed, start change picture and audio
+  selection.addEventListener('change', (event) => {
+    //console.log(event.target.value);
+    // change picture source
+    hornImg.setAttribute('src', `assets/images/${event.target.value}.svg`);
+    // change audio source
+    audio.setAttribute('src', `assets/audio/${event.target.value}.mp3`);
+  });
 }
