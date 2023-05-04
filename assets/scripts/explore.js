aa20a5a9e65dc3d4d8e9d1b4b835704a6cdb6a15
TerryYan26 <103209100+TerryYan26@users.noreply.github.com>
2023-05-04 15:26:03 -0700

    expose leave Party Horn not done and explore done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -6,60 +6,63 @@ function init() {
   const voiceSelect = document.querySelector("select");
   const inputTxt = document.querySelector("textarea");
   const speakBtn = document.querySelector("button");
+const faceImg = document.querySelector("img");
+  faceImg.setAttribute("src", "assets/images/smiling.png");
+  const exploreSection = document.querySelector("#explore");
+  exploreSection.insertBefore(faceImg, exploreSection.firstChild);
 
   // initialize a SpeechSynthesis
   const synth = window.speechSynthesis;
 
   let voices = [];
+  let selectedVoice;
+
   function populateVoiceList() {
     // Returns a list of SpeechSynthesisVoice objects representing all the available voices on the current device.
     voices = synth.getVoices();
-    //console.log(voices.length);
-  
+
     for (let i = 0; i < voices.length; i++) {
       const option = document.createElement("option");
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
-  
+
       if (voices[i].default) {
         option.textContent += " — DEFAULT";
       }
-  
+
       option.setAttribute("data-lang", voices[i].lang);
       option.setAttribute("data-name", voices[i].name);
       voiceSelect.appendChild(option);
     }
   }
-  //populateVoiceList();
-  
+
   // check whether the onvoiceschanged event is supported by the browser
   if (speechSynthesis.onvoiceschanged !== undefined) {
     // when the onvoiceschanged event is triggered, the populateVoiceList callback function will be called
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
-
-  let selectedVoice;
-  voiceSelect.addEventListener('change', (event) =>{
-    // get the name of the selected voice
-    const selectedOption = event.target.selectedOptions[0].getAttribute("data-name")
-    //console.log(selectedOption);
-    // find the selected voice in the voices array, and apply to the speech
-    for (let i = 0; i < voices.length; i++) {
-      if (voices[i].name === selectedOption) {
-        //console.log(voices[i].name);
-        selectedVoice = voices[i];
-      }
-    }
-
-  })
-
   speakBtn.addEventListener('click',() => {
-    //console.log('pressed');
     const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
 
     utterThis.voice = selectedVoice;
     synth.speak(utterThis);
-  
+    faceImg.setAttribute("src", "assets/images/smiling-open.png");
+
+    utterThis.onend = function() {
+      faceImg.setAttribute("src", "assets/images/smiling.png");
+    };
     inputTxt.blur();
-  })
+  });
+
+  voiceSelect.addEventListener('change', (event) =>{
+    // get the name of the selected voice
+    const selectedOption = event.target.selectedOptions[0].getAttribute("data-name")
+
+    // find the selected voice in the voices array, and apply to the speech
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        selectedVoice = voices[i];
+      }
+    }
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,5 +1,7 @@
 // expose.js
 
+
+
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
@@ -9,34 +11,40 @@ function init() {
   const control = document.getElementById('volume');
   const audio = document.querySelector('audio');
   const icon = document.querySelector('#volume-controls img');
-
+  const button = document.querySelector('button');
 
   // Change picture and audio after horn selection
   selection.addEventListener('change', (event) => {
-    //console.log(event.target.value);
     // change picture source
     hornImg.setAttribute('src', `assets/images/${event.target.value}.svg`);
     // change audio source
     audio.setAttribute('src', `assets/audio/${event.target.value}.mp3`);
+
+    if (event.target.value === "party-horn") {
+      confetti();
+    }
   });
 
-  // Change volumn icon based on control's value
-  control.addEventListener('change',() => {
+  // Play sound on button click
+  button.addEventListener('click', () => {
+    audio.volume = control.value / 100; // set the volume based on the control value
+    audio.play();
+  });
+
+  // Change volume icon and adjust volume level based on control's value
+  control.addEventListener('input', () => {
     let volume = control.value;
     // change volume
-    audio.setAttribute('volumn', `${volume/100}`);
+    audio.volume = volume / 100;
     // change icon
-    if(volume <= 0){
+    if (volume <= 0) {
       icon.setAttribute('src', 'assets/icons/volume-level-0.svg');
-    }
-    else if(1 <= volume && volume < 33){
+    } else if (volume < 33) {
       icon.setAttribute('src', 'assets/icons/volume-level-1.svg');
-    }
-    else if(33 <= volume && volume < 67){
+    } else if (volume < 67) {
       icon.setAttribute('src', 'assets/icons/volume-level-2.svg');
-    }
-    else if(67 <= volume){
+    } else {
       icon.setAttribute('src', 'assets/icons/volume-level-3.svg');
     }
-  })
+  });
 }
